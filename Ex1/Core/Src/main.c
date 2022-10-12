cb4e675f74e71811f8cf009736b1ad13fd247048
Quang Phuc <phucquangle0503@gmail.com>
2022-10-12 09:49:20 +0700

    commit ex5

--- Ex1/Core/Src/main.c
+++ Ex1/Core/Src/main.c
@@ -59,6 +59,7 @@ static void MX_TIM2_Init(void);
 const int MAX_LED = 4;
 int index_led = 0;
 int led_buffer[4] = {1 ,2, 3, 4};
+int hour = 15, minute = 8, second = 50;
 void update7SEG(int index){
 	switch(index){
 	case 0:
@@ -134,6 +135,13 @@ void display7SEG(int num){
 	 }
  }
 
+void updateClockBuffer(){
+	led_buffer[0] = hour/10;
+	led_buffer[1] = hour%10;
+	led_buffer[2] = minute/10;
+	led_buffer[3] = minute%10;
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -171,8 +179,23 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
   while (1)
   {
+	  second++;
+	  if(second >= 60){
+		  second = 0;
+		  minute++;
+	  }
+	  if(minute >= 60){
+		  minute = 0;
+		  hour++;
+	  }
+	  if(hour >= 24){
+		  hour = 0;
+	  }
+	  updateClockBuffer();
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -304,7 +327,6 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 int counter = 25;
 int counter1 = 100;
-//int status = 0;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	if(counter > 0){
@@ -313,48 +335,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	if(counter <= 0){
 		counter = 25;
 		//TODO
-		//HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-		//HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 		update7SEG(index_led++);
 		if(index_led > 3)
 			index_led = 0;
-//		switch (status % 4) {
-//			case 0:
-//			{
-//				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-//				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-//				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
-//				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-//				display7SEG(1);
-//			}
-//				break;
-//			case 1:
-//			{
-//				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-//				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
-//				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
-//				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-//				display7SEG(2);
-//			}
-//				break;
-//			case 2:
-//				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-//				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-//			    HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
-//				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-//				display7SEG(3);
-//				break;
-//			case 3:
-//				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-//				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-//				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
-//				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
-//				display7SEG(0);
-//				break;
-//			default:
-//				break;
-//		}
-//		status++;
 	}
 	if(counter1 > 0){
 		counter1--;
@@ -362,7 +345,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	if(counter1 <= 0){
 		counter1 = 100;
 		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
-
 	}
 	//timerRun();
 }
